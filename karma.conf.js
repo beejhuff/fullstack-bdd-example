8b9fe87624579702ed89c16ba48297e1d7bd9e2d
Alistair Stead <alistair.stead@designdisclosure.com>
2014-08-15 10:56:43 +0100

    Add tool configuration and dependencies

---
+++ karma.conf.js
@@ -0,0 +1,91 @@
+// Karma configuration
+// Generated on Wed Dec 11 2013 15:10:10 GMT+0000 (GMT)
+
+module.exports = function(config) {
+  config.set({
+
+    // base path, that will be used to resolve files and exclude
+    basePath: '',
+
+
+    // frameworks to use
+    frameworks: ['mocha', 'chai', 'sinon'],
+
+
+    // list of files / patterns to load in the browser
+    files: [
+      'bower_components/angular/angular.js',
+      'bower_components/angular-mocks/angular-mocks.js',
+      'public/js/application.js',
+      'public/js/components/**/*.js',
+      'public/js/components/**/*.html'
+    ],
+
+
+    // list of files to exclude
+    exclude: [
+
+    ],
+
+
+    // test results reporter to use
+    // possible values: 'dots', 'progress', 'junit', 'growl', 'coverage'
+    reporters: ['spec'],
+
+
+    // web server port
+    port: 9876,
+
+
+    // enable / disable colors in the output (reporters and logs)
+    colors: true,
+
+
+    // level of logging
+    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+    logLevel: config.LOG_INFO,
+
+    preprocessors: {
+      'public/js/components/**/*.html': 'ng-html2js'
+    },
+
+    ngHtml2JsPreprocessor: {
+      // strip this from the file path
+      stripPrefix: 'public/',
+      // prepend this to the
+      prependPrefix: '',
+
+      // or define a custom transform function
+      // cacheIdFromPath: function(filepath) {
+      //   return cacheId;
+      // },
+
+      // setting this option will create only a single module that contains templates
+      // from all the files, so you can load them all with module('foo')
+      moduleName: 'mock-templates'
+    },
+
+    // enable / disable watching file and executing tests whenever any file changes
+    autoWatch: true,
+
+
+    // Start these browsers, currently available:
+    // - Chrome
+    // - ChromeCanary
+    // - Firefox
+    // - Opera
+    // - Safari (only Mac)
+    // - PhantomJS
+    // - IE (only Windows)
+    browsers: ['ChromeCanary'],
+
+
+    // If browser does not capture in given timeout [ms], kill it
+    captureTimeout: 60000,
+
+
+    // Continuous Integration mode
+    // if true, it capture browsers, run tests and exit
+    singleRun: false
+  });
+};
---
+++ protractor.conf.js
@@ -0,0 +1,59 @@
+var debug = require('debuglog')('protractor')
+  , app = require('express')();
+  
+exports.config = {
+  // The timeout for each script run on the browser. This should be longer
+  // than the maximum time your application needs to stabilize between tasks.
+  allScriptsTimeout: 11000,
+
+  specs: [
+    'features/*.feature',
+  ],
+
+  capabilities: {
+    'browserName': 'chrome'
+  },
+
+  // ----- More information for your tests ----
+  //
+  // A base URL for your application under test. Calls to protractor.get()
+  // with relative paths will be prepended with this.
+  baseUrl: 'http://localhost:8000',
+
+  // A callback function called once protractor is ready and available, and
+  // before the specs are executed
+  // You can specify a file containing code to run by setting onPrepare to
+  // the filename string.
+  onPrepare: function() {
+    app.listen(8000, function() {
+      debug('Protractor mock server listening on http://localhost:%d', 8000);
+    });
+  },
+
+  // The params object will be passed directly to the protractor instance,
+  // and can be accessed from your test. It is an arbitrary object and can
+  // contain anything you may need in your test.
+  // This can be changed via the command line as:
+  //   --params.login.user 'Joe'
+  // params: { shop: shop },
+
+  // ----- The test framework -----
+  //
+  // Jasmine and Cucumber are fully supported as a test and assertion framework.
+  // Mocha has limited beta support. You will need to include your own
+  // assertion framework if working with mocha.
+  framework: 'cucumber',
+
+  // ----- Options to be passed to cucumber -----
+  cucumberOpts: {
+    // Only execute the features or scenarios with tags matching @dev.
+    // This may be an array of strings to specify multiple tags to include.
+    tags: '@server',
+
+    // How to format features (default: progress)
+    format: 'pretty'
+  },
+
+};
+
+process.env.WORLD = 'ui';
